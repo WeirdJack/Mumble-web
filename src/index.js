f02f8452bcdf9955e1b8dfab98df19677910dd35
WeirdJackgit config --global user.email 17jack92@gmail.com <32279932+WeirdJack@users.noreply.github.com>
2021-03-31 14:37:52 -0400

    changes Axios to axios in index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Axios from 'axios'
+import axios from 'axios'
 
 if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
   // development URL

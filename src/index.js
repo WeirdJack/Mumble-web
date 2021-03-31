13ab77150a4e5e047931d68a645135954abd655e
WeirdJackgit config --global user.email 17jack92@gmail.com <32279932+WeirdJack@users.noreply.github.com>
2021-03-31 14:34:43 -0400

    added axios import to index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Axios from 'axios'
 
 if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
   // development URL
-  axios.defaults.baseURL = `${process.env.REACT_APP_API_SERVER_DEVELOPMENT}/api/v1`;
+  axios.defaults.baseURL = `${process.env.REACT_APP_API_SERVER_DEVELOPMENT}/`;
 } else {
   // production URL
-  axios.defaults.baseURL = `${process.env.REACT_APP_API_SERVER_PRODUCTION}/api/v1`;
+  axios.defaults.baseURL = `${process.env.REACT_APP_API_SERVER_PRODUCTION}/`;
 }
 
 ReactDOM.render(
